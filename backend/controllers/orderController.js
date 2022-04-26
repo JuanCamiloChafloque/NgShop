b5c9b01f50abbedd7a21f41cf6cbb38b7a3d8787
Camilo <camilochafloque@gmail.com>
2022-04-26 16:44:32 -0500

    Create order functionality

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,3 +1,8 @@
 const Order = require("../models/Order");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
+exports.createOrder = catchAsyncErrors(async (req, res, next) => {
+  const newOrder = await Order.create(req.body);
+  res.status(201).json({ success: true, order: newOrder });
+});
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 //Controllers
-const {} = require("../controllers/orderController");
+const { createOrder } = require("../controllers/orderController");
 
 // Auth Middlewares
 const {
   isAuthenticatedUser,
   isAuthorizedRoles,
 } = require("../middleware/auth");
+
+router.route("/").post(isAuthenticatedUser, createOrder);
 
 module.exports = router;
