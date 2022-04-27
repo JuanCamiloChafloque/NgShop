8a57b11e55d8ce4d9ab8dbc977243ec3ffaed969
Camilo <camilochafloque@gmail.com>
2022-04-26 19:48:27 -0500

    Get orders and order by id done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,3 +6,27 @@ exports.createOrder = catchAsyncErrors(async (req, res, next) => {
   const newOrder = await Order.create(req.body);
   res.status(201).json({ success: true, order: newOrder });
 });
+
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({})
+    .populate(["orderItems.product", "user"])
+    .sort({ dateOrdered: -1 });
+  if (!orders) {
+    return next(new ErrorHandler("There are no orders registered.", 404));
+  }
+  res.status(200).json({ success: true, count: orders.length, orders: orders });
+});
+
+exports.getOrderById = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const order = await Order.findById(id).populate([
+    "orderItems.product",
+    "user",
+  ]);
+  if (!order) {
+    return next(
+      new ErrorHandler("The order with id " + id + " does not exist", 404)
+    );
+  }
+  res.status(200).json({ success: true, order: order });
+});
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 //Controllers
-const { createOrder } = require("../controllers/orderController");
+const {
+  createOrder,
+  getOrderById,
+  getAllOrders,
+} = require("../controllers/orderController");
 
 // Auth Middlewares
 const {
@@ -10,6 +14,11 @@ const {
   isAuthorizedRoles,
 } = require("../middleware/auth");
 
-router.route("/").post(isAuthenticatedUser, createOrder);
+router
+  .route("/")
+  .get(isAuthenticatedUser, isAuthorizedRoles(), getAllOrders)
+  .post(isAuthenticatedUser, createOrder);
+
+router.route("/:id").get(isAuthenticatedUser, getOrderById);
 
 module.exports = router;
