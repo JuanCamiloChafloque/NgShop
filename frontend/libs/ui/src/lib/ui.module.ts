3b0a66cc6b81a44d7880b3d82d330681080f8340
Camilo <camilochafloque@gmail.com>
2022-05-02 10:34:58 -0500

    Banner for home page done

--- frontend/libs/ui/src/lib/ui.module.ts
+++ frontend/libs/ui/src/lib/ui.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { BannerComponent } from './banner/banner.component';
 import { SliderComponent } from './slider/slider.component';
+import { ButtonModule } from 'primeng/button';
 
 export const uiRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ButtonModule],
   declarations: [BannerComponent, SliderComponent],
   exports: [BannerComponent, SliderComponent],
 })
